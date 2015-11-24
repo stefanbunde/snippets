c893afa8c9eed6dea6a05689ecd2f3860d7cba3f
Stefan Bunde <stefanbunde@gmail.com>
2015-11-24 07:44:35 +0100

    Add bubble sort

--- sort_algorithms/python/src/bubble_sort.py
+++ sort_algorithms/python/src/bubble_sort.py
@@ -1,2 +1,10 @@
 def bubble_sort(items):
+    swapped = True
+
+    while swapped:
+        swapped = False
+        for i in range(1, len(items)):
+            if items[i-1] > items[i]:
+                items[i-1], items[i] = items[i], items[i-1]
+                swapped = True
     return items
