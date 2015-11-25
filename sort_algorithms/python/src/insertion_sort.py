17c1fc1fa475fb7633a078c6703531c5d47b9d2a
Stefan Bunde <stefanbunde@gmail.com>
2015-11-25 06:37:29 +0100

    Add optimized insertion sort

--- sort_algorithms/python/src/insertion_sort.py
+++ sort_algorithms/python/src/insertion_sort.py
@@ -1,4 +1,4 @@
-__all__ = "insertion_sort",
+__all__ = "insertion_sort", "insertion_sort_optimized"
 
 
 def insertion_sort(items):
@@ -7,4 +7,15 @@ def insertion_sort(items):
         while j > 0 and items[j-1] > items[j]:
             items[j], items[j-1] = items[j-1], items[j]
             j -= 1
+    return items
+
+
+def insertion_sort_optimized(items):
+    for i in range(1, len(items)):
+        x = items[i]
+        j = i
+        while j > 0 and items[j-1] > x:
+            items[j] = items[j-1]
+            j -= 1
+        items[j] = x
     return items
